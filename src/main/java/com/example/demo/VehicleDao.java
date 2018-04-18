2993b3ecfb52dbb2a8d99f5b25915045db289860
ACGartland <agartlan@ggc.edu>
2018-04-18 17:08:50 -0400

    Updated Dependencies again

--- src/main/java/com/example/demo/VehicleDao.java
+++ src/main/java/com/example/demo/VehicleDao.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 @Transactional
